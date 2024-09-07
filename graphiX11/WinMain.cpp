237569cce04567347618169029850ca110af1a6d
Shubham More <shubham.a.more20@gmail.com>
2024-09-07 10:57:48 -0700

    Encapsulated windows code into its own class

--- graphiX11/WinMain.cpp
+++ graphiX11/WinMain.cpp
@@ -1,16 +1,6 @@
-#include <Windows.h>
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	}
-	return DefWindowProc(hwnd, msg, wParam, lParam);
-}
+#include "X11Win.h"
+#include "Window.h"
+#include <sstream>
 
 
 int CALLBACK WinMain(
@@ -19,34 +9,7 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	const auto pClassName = "GraphiX11";
-	//register Windows class
-	WNDCLASSEX wc = { 0 };
-
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-
-	RegisterClassEx(&wc);
-
-	// create window instance
-	HWND hwnd = CreateWindowEx(
-		0, pClassName,
-		"GraphiX11 Engine",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr);
-
-	ShowWindow(hwnd, SW_SHOW);
+	Window wnd(800, 300, "Graphix11");
 
 	MSG msg;
 	BOOL gResult;
